27f64728606e5c0050fb3a655c2303797ebd8f78
islam <sanayatov.islam@gmail.com>
2020-04-16 16:02:46 +0600

    'init'

---
+++ impl/src/test/java/kz/pashim/enroute/examples/quickstart/rest/UnitTest.java
@@ -0,0 +1,12 @@
+package kz.pashim.enroute.examples.quickstart.rest;
+
+import org.junit.Test;
+
+public class UnitTest {
+    
+    @Test
+    public void testSomething() {
+        //TODO add an implementation
+    }
+    
+}
